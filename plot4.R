2e2237e0c6b55adb045dc69719d466d3b2dfad59
Rahul Aranha <rahularanha@gmail.com>
2017-11-22 21:04:23 +0530

    plot4 R code

--- plot4.R
+++ plot4.R
@@ -24,7 +24,7 @@ plot(datetime, subsetTab$Sub_metering_1, type = "l", col="black",
 lines(datetime, subsetTab$Sub_metering_2, type = "l", col="red")
 lines(datetime, subsetTab$Sub_metering_3, type = "l", col="blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lty=1, lwd=1.5, col = c("black", "red", "blue"), bty = "n")
+       lty=1, lwd=1, col = c("black", "red", "blue"), bty = "n")
 
 plot(datetime, subsetTab$Global_reactive_power, type = "l",
      xlab = "datetime", ylab = "Global_reactive_power")
