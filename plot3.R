7b8f412a306009895206dbfb78a168e04b0873de
Rahul Aranha <rahularanha@gmail.com>
2017-11-22 20:42:52 +0530

    plot3 R code

--- plot3.R
+++ plot3.R
@@ -1,6 +1,5 @@
-clas <- c(Voltage="numeric", Global_active_power="numeric",Global_intensity="numeric",Sub_metering_1="numeric",Sub_metering_2="numeric",Sub_metering_3="numeric",Global_active_power="numeric",Global_reactive_power="numeric")
 
-mainTab <- read.table("household_power_consumption.txt", sep = ";", dec=".", stringsAsFactors=FALSE, header = TRUE, na.strings = "?", colClasses=clas)
+mainTab <- read.table("household_power_consumption.txt", sep = ";", dec=".", stringsAsFactors=FALSE, header = TRUE, na.strings = "?")
 
 #subsetting main table for the required two dates
 subsetTab <- mainTab[mainTab$Date %in% c("1/2/2007","2/2/2007") ,]
