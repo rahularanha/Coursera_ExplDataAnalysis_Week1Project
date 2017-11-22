ade94a45c0742c3c456fc330500403442220b6d0
Rahul Aranha <rahularanha@gmail.com>
2017-11-22 17:08:56 +0530

    Plot2 R code

---
+++ plot2.R
@@ -0,0 +1,17 @@
+clas <- c(Voltage="numeric", Global_active_power="numeric",Global_intensity="numeric",Sub_metering_1="numeric",Sub_metering_2="numeric",Sub_metering_3="numeric",Global_active_power="numeric",Global_reactive_power="numeric")
+
+mainTab <- read.table("household_power_consumption.txt", sep = ";", dec=".", stringsAsFactors=FALSE, header = TRUE, na.strings = "?")
+
+#subsetting main table for the required two dates
+subsetTab <- mainTab[mainTab$Date %in% c("1/2/2007","2/2/2007") ,]
+
+as.Date(subsetTab$Date)             # changing date vector to appropriate convention
+
+subsetTab <- na.omit(subsetTab)
+# changing time vector to appropriate convention
+datetime <- strptime(paste(subsetTab$Date, subsetTab$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+png("plot2.png", width=480, height=480)
+plot(datetime, subsetTab$Global_active_power, type = "l",
+     xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
