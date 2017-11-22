ec9ee4e25bba307e36db0c7ce0310b518cca33c5
Rahul Aranha <rahularanha@gmail.com>
2017-11-22 20:47:08 +0530

    plot3 R code

--- plot3.R
+++ plot3.R
@@ -16,5 +16,5 @@ plot(datetime, subsetTab$Sub_metering_1, type = "l", col="black",
 lines(datetime, subsetTab$Sub_metering_2, type = "l", col="red")
 lines(datetime, subsetTab$Sub_metering_3, type = "l", col="blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lty=1, lwd=2.5, col = c("black", "red", "blue"))
+       lty=1, lwd=1, col = c("black", "red", "blue"))
 dev.off()
